b7e22e299ba13dd737a2f342343f79fca6dfe93e
Arkansyahputra <arkansyahputra701@gmail.com>
2025-11-30 22:01:11 +0700

    fix: fix error plugin

--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -1,30 +1,14 @@
 return {
-    "brianhuster/live-preview.nvim",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-    },
-    event = "VeryLazy",
-    opts = {
-        port = 5500,
-        open_window = true,
-    },
-    keys = {
-        {
-            "<leader>lp",
-            function()
-                require("live-preview").start()
-            end,
-            desc = "Live Preview: Start",
-        },
-        {
-            "<leader>lP",
-            function()
-                require("live-preview").stop()
-            end,
-            desc = "Live Preview: Stop",
-        },
-    },
-    config = function(_, opts)
-        require("live-preview").setup(opts)
-    end,
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  event = "VeryLazy",
+  opts = {
+    port = 5500,
+    open_window = true,
+  },
+  config = function(_, opts)
+    require("live-preview").setup(opts)
+  end,
 }
